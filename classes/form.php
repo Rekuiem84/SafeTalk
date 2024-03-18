0917c4a46428bf9f1a9fad215afce6df908c3314
Rekuiem84 <morinijoshua@gmail.com>
2024-03-18 16:16:47 +0100

    initial commit

---
+++ classes/form.php
@@ -0,0 +1,50 @@
+<?php
+
+class Form
+{
+    // Cette fonction vérifie si le formulaire a été soumis 
+    public function isSubmitted(): bool
+    {
+        return !empty($_POST);
+    }
+
+    // Cette fonction vérifie tous les champs du formulaire de login sont remplis
+    public function isValidLoginForm(): bool
+    {
+        return (!empty($_POST["email"]) && !empty($_POST["password"]));
+    }
+
+    // Cette fonction renvoie un array avec les erreurs de validation du formulaire
+    public function getErrorsLogin(): array
+    {
+        $errors = [
+            "email" => empty($_POST["email"]) ? "Merci de renseigner votre email" : "",
+            "mdp" => empty($_POST["password"]) ? "Merci de renseigner votre mot de passe" : ""
+        ];
+        return $errors;
+    }
+
+    // Cette fonction vérifie si les champs spécifiés dans le paramètre $params sont valides (non vides)
+    // $params est un array
+    public function isValidAnyForm($params): bool
+    {
+        foreach ($params as $param) {
+            if (empty($param)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Renvoie un array avec toutes les données de l'utilisateur
+    public function getUserData($email): array
+    {
+        $db = new Db;
+        $co = $db->dbCo("panza", "root", "root");
+
+        $sql = "SELECT * FROM `membre` WHERE email = ?";
+        $param = [$email];
+        $result = $db->SQLWithParam($sql, $param, $co);
+        return $result;
+    }
+}
---
+++ classes/login.php
@@ -0,0 +1,52 @@
+<?php
+
+class Login
+{
+
+    private $id;
+
+    private $email;
+
+    private $mdp;
+
+
+    public function getID()
+    {
+        return $this->id;
+    }
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    public function getMdp()
+    {
+        return $this->mdp;
+    }
+
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+    public function setMotdepasse($mdp)
+    {
+        $this->mdp = $mdp;
+    }
+
+    /* Doit renvoyer un bool*/
+    public function checkAccess($email, $mdp)
+    {
+        $db = new Db;
+        $co = $db->dbCo("panza", "root", "root");
+
+        $sql = "SELECT `email`, SHA1(`mot_de_passe`) mot_de_passe FROM `membre` WHERE email = ? AND mot_de_passe = ?";
+        $param = [$email, sha1($mdp)];
+        $result = $db->SQLWithParam($sql, $param, $co);
+        return !empty($result);
+    }
+
+    public function connect()
+    {
+        header("Location: dashboard.php");
+    }
+}
